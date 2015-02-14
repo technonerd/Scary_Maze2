bc526fd83214deb91d9e3da137c27dec77f9c9cf
koprty <koprty@users.noreply.github.com>
2015-02-14 18:53:51 -0500

    server and client done; note socket bugs

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 var app = require ("express")();
 var http = require ("http").Server(app);
 var io = require("socket.io")(http);
@@ -8,7 +6,7 @@ var path = require("path")
 var total = 0;
 var webpage = "index.html";
 var webdir = "templates/"
-
+var onewinner= false;
 app.get("/", function(re,res){
     res.sendFile(path.join(__dirname,webdir,webpage));
 });
@@ -20,11 +18,26 @@ io.on("connection", function(socket){
 	total--;
 	console.log('a user disconnected');
     });
-});
+    socket.on('winner', function (data) {
+	if (!onewinner){
+	    io.emit("close", {close:"close"});
+	    onewinner=true;
+	    console.log("closing");
+	}
+    });
+    socket.on("restart", function(){
+	onewinner=false;
+	io.emit("new_level","new_levelled");
 
+	console.log("restarted");
+    });
+});
+      
 http.listen(3000, function(){
     console.log("listening on *:3000");
 });
 
 
 
+
+//bugs to note -> automatic disconnection when window is not main window
