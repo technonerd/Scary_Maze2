98009cd8c8746e0734a4aca36dbe5fac437482c5
echenran <ecr@ECRs-MacBook-Air.local>
2015-02-14 19:37:04 -0500

    maze update, scripts update, and moving div

--- Scripts.js
+++ Scripts.js
@@ -1,7 +1,33 @@
-(function($){
+/*(function($){
     $(document).ready(function ($) {
-        $(".maze").mouseleave(function(){
+        /*$(".maze").mouseleave(function(){
+            $('.myBox').is(':hover') === true
             $(".maze").css("background-color", "black");
+        });
+
+        $('.start').click(function(){
+            $(".start").css("margin-left","150px");
+            $(".start").css("margin-top","500px");
         });
     });
 })(jQuery);
+
+$(document).ready(function(){
+  // when the buttom is clicked
+  $('.start').click(function(){
+    // get the left position of div2
+    var div2Left = $('#div2').position().left
+    // animate so that the right side of div1 matches div2's left
+    $('#div1').animate({
+      'right': '250px',
+    }, {
+        // this is called every step of the animation 
+        step: function(currentRightPos) {
+          // check if collision occurs
+          if (currentRightPos >= div2Left) {
+            console.log('The divs collided!');
+          }
+        }
+    });
+  });
+});*/
---
+++ mov.js
@@ -0,0 +1,39 @@
+window.onload = function () {
+    var moveDiv = document.getElementById("movingDiv");
+    window.onkeydown = function(e) {
+        e.preventDefault();
+        if (!e){
+            e = window.event;
+        }
+        var keyCode;
+        // pixel wise speed variable
+        var speed = 10;      
+		if (e.which) {
+            keyCode = e.which;
+        } else {
+            keyCode = e.keyCode;
+        }
+
+//increment/decrement the top or left of the div based on the arrow key movements
+
+        if(keyCode === 37) {
+            moveDiv.style.left = (parseInt(moveDiv.style.left, 10) - speed) + 'px';
+        } else if (keyCode === 38) {
+           moveDiv.style.top = (parseInt(moveDiv.style.top, 10) - speed) + 'px';
+        } else if (keyCode === 39) {
+           moveDiv.style.left = (parseInt(moveDiv.style.left, 10) + speed) + 'px';
+        } else if (keyCode === 40) {
+            moveDiv.style.top = (parseInt(moveDiv.style.top, 10) + speed) + 'px';
+        }
+    };
+	
+	/*function checkcollision() {
+		var imgd = ctx.getImageData(x, y, 15, 15);
+		var pix = imgd.data;
+		for (var i = 0; n = pix.length, i < n; i += 4) {
+			if (pix[i] == 0) {
+				collision = 1;
+			}
+		}
+	}*/
+};
